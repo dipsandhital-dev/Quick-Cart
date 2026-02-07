dc4e0589c34463a468ea55f009810685d0213420
Dipsan Dhital <dipsandhital12@gmail.com>
2026-02-07 08:08:32 +0545

    Setting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Orders from './pages/Orders'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+
+
 
 const App = () => {
   return (
-     <div className=''> 
-
-     
-    </div>
+     <>
+     <Router>
+      <Routes>
+        <Route  path='/' element= {<Home/>} />
+        <Route  path= '/auth' element= {<Auth/>} />
+        <Route path= '/orders' element= {<Orders/>} />
+        </Routes>
+     </Router>
+     </>
   )
 }
 
